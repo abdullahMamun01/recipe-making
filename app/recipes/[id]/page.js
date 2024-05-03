53bf36e71956edddcd00c6d046454bfebb459d11
unknown <abdullah.mamun.0110@gmail.com>
2024-05-03 23:35:35 +0600

    pushing for deploying issue

--- app/recipes/[id]/page.js
+++ app/recipes/[id]/page.js
@@ -3,27 +3,27 @@ import RecipeDetails from "@/components/recipes/RecipeDetails";
 import RecipeSteps from "@/components/recipes/RecipeSteps";
 import { getRecipeById } from "@/db/queries";
 
-export async function generateMetadata({ params:{id} }, parent) {
+// export async function generateMetadata({ params:{id} }, parent) {
 
-  const recipe =  await getRecipeById(id) 
+//   const recipe =  await getRecipeById(id) 
 
 
-  const previousImages = (await parent).openGraph?.images || [];
-  return {
-      title: recipe?.name,
-      description: recipe?.description.slice(0, 100),
-      openGraph: {
-          images: [
+//   const previousImages = (await parent).openGraph?.images || [];
+//   return {
+//       title: recipe?.name,
+//       description: recipe?.description.slice(0, 100),
+//       openGraph: {
+//           images: [
      
-              {
-                  url: 'https://recipe-making.vercel.app/api/og',
-                  width: 1200,
-                  height: 600,
-              },
-          ],
-      },
-  };
-}
+//               {
+//                   url: 'https://recipe-making.vercel.app/api/og',
+//                   width: 1200,
+//                   height: 600,
+//               },
+//           ],
+//       },
+//   };
+// }
 
 export default async function RecipesDetailsPage({params: {id}}) {
   const {steps,...recipe } =  await getRecipeById(id) 
--- components/auth/LoginForm.jsx
+++ components/auth/LoginForm.jsx
@@ -3,42 +3,51 @@
 import { performLogin } from '@/app/actions'
 import { useAuth } from '@/app/hooks/useAuth';
 import { useRouter } from 'next/navigation';
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginForm() {
-    const router  = useRouter()
-    const {setAuth} = useAuth()
-    const onSubmit =  async (e) => {
+    const router = useRouter()
+    const { setAuth } = useAuth()
+    const [error, setError] = useState("")
+    const onSubmit = async (e) => {
         e.preventDefault()
-        
+
         try {
             const formData = new FormData(e.currentTarget)
-            const user  = await performLogin(formData)
-            if(user){
+            const user = await performLogin(formData)
+            if (user) {
                 router.push('/')
                 setAuth(user)
                 console.log(user)
+                setError('')
+            }else{
+                setError('invalid credential')
             }
 
         } catch (error) {
+           
             console.log(error)
         }
     }
 
 
     return (
-        <form className="login-form" onSubmit={onSubmit}>
-            <div>
-                <label htmlFor="email">Email Address</label>
-                <input type="email" name="email" id="email" />
-            </div>
+        <div>
+            <form className="login-form" onSubmit={onSubmit}>
+                <div>
+                    <label htmlFor="email">Email Address</label>
+                    <input type="email" name="email" id="email" required/>
+                </div>
 
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
-            </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" required />
+                </div>
 
-            <button type="submit" className="bg-[#eb4a36] py-3 rounded-md text-white w-full mt-4">Login</button>
-        </form>
+                <button type="submit" className="bg-[#eb4a36] py-3 rounded-md text-white w-full mt-4">Login</button>
+            </form>
+
+            <span className='text-red-600 my-5'>{error}</span>
+        </div>
     )
 }
