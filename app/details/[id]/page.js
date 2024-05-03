5df33c75cb0b8141fcaa6466b3934835b77bb67b
unknown <abdullah.mamun.0110@gmail.com>
2024-05-03 11:48:41 +0600

    change something

--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -3,6 +3,30 @@ import RecipeDetails from "@/components/recipes/RecipeDetails";
 import RecipeSteps from "@/components/recipes/RecipeSteps";
 import { getRecipeById } from "@/db/queries";
 
+export async function generateMetadata({ params:{id}, searchParams }, parent) {
+  const {name , description,image} =  await getRecipeById(id) 
+  console.log({name})
+  const previousImages = (await parent).openGraph?.images || [];
+  return {
+      title: name,
+      description: description.slice(0, 100),
+      openGraph: {
+          images: [
+
+            {
+              url: image,
+              width: 1200,
+              height: 600,
+          },
+              {
+                  url: `https://recipe-making.vercel.app/api/og`,
+                  width: 1200,
+                  height: 600,
+              },
+          ],
+      },
+  };
+}
 
 export default async function RecipesDetailsPage({params: {id}}) {
   const {steps,...recipe } =  await getRecipeById(id) 
